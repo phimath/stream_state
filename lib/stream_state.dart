57927cf4a39bb402aa173d910f9a2bf7496d99bd
zak <zacharydimaria@gmail.com>
2020-08-01 13:38:02 -0700

    start adding api docs

--- lib/stream_state.dart
+++ lib/stream_state.dart
@@ -3,7 +3,12 @@ library stream_state;
 import 'package:flutter/material.dart';
 import 'dart:async';
 
+/// A thin wrapper around streams to act as a simple state management solution.
+/// While you dont need to provide [<T>] its good practice to do so.
+/// Each [StreamState] object manages a single variable / value of type [<T>]
+/// [StreamState] objects can manage any class or type.
 class StreamState<T> {
+  /// initial value of this object
   final T initial;
   T _current;
 
@@ -11,6 +16,7 @@ class StreamState<T> {
 
   Stream stream;
 
+  ///Build a StreamState with an [initial] value.
   StreamState({@required this.initial}) {
     _current = initial;
     stream = _streamController.stream.asBroadcastStream();
